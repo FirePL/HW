a3bfa9c4506c5255741f775f578418b0c5ce1796
FirePL <94689867+FirePL@users.noreply.github.com>
2021-11-29 11:36:43 +0200

    Add files via upload

---
+++ BMI.py
@@ -0,0 +1,4 @@
+weight_in_kilogram = input('Enter your weight in kg:')
+height_in_meters = input('Enter your height in meters:')
+BMI = weight_in_kilogram / (height_in_meters*height_in_meters)
+print(round(BMI,2))
---
+++ acad_to_astro_hours.py
@@ -0,0 +1,3 @@
+astro_hour = 60
+course = 64*2/3*astro_hour+((64-1)/3)*1/4*astro_hour
+print(course)
