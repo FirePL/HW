e78f4e31017da78a943d5201e43592ea6fa54cde
FirePL <94689867+FirePL@users.noreply.github.com>
2021-12-21 12:26:38 +0200

    Add files via upload

--- users_bills.py
+++ users_bills.py
@@ -1,6 +1,18 @@
-clients = {  (1,2,3), ("Maria", "Ivan", "Asen"), ("+39587111111", "+39587222222", "+39587333333") }
-bills = { (1,2,3), (25.50, 30.48, 5.98) }
+bills = {1: 25.5, 2:30.48, 3: 5.98}
+clients_names = {1: 'Maria', 2:'Ivan', 3: 'Asen'}
 
-clients_bills = clients.union(bills)
+max_bills_dict = { bills_key: bills_value for bills_key, bills_value in list(bills.items()) if bills_value == max(bills.values())}
 
-print(clients_bills[1])
+for key, value in max_bills_dict.items():
+    max_key = ("{}".format(key))
+    max_bill =  ("{}".format(value))
+
+print(f'The user with highest bill - {max_bill} is {clients_names[int(max_key)]}')
+
+min_bills_dict = { bills_key: bills_value for bills_key, bills_value in list(bills.items()) if bills_value == min(bills.values())}
+
+for key, value in min_bills_dict.items():
+    min_key = ("{}".format(key))
+    min_bill =  ("{}".format(value))
+
+print(f'The user with lowest bill - {min_bill} is {clients_names[int(min_key)]}')
